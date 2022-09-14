9bb0403dccb44479987aeb65fe75e8097886407d
Tiago Tavares <bigtavadev@gmail.com>
2022-09-14 14:52:23 +0100

    refactor: products page dispatching action

--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -1,18 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { Product } from "../../app/api";
 
 export interface ProductState {
   products: { [id: string]: Product };
 }
 
-const initialState: ProductState = {
-  products: {},
-};
+const initialState: ProductState = { products: {} };
 
 const productSlice = createSlice({
   name: "products",
   initialState,
-  reducers: {},
+  reducers: {
+    receivedProducts(state, action: PayloadAction<Product[]>) {
+      const products = action.payload;
+
+      products.forEach((product) => {
+        state.products[product.id] = product;
+      });
+    },
+  },
 });
 
+export const { receivedProducts } = productSlice.actions;
 export default productSlice.reducer;
